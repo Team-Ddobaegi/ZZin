8313f96584cbb0ccd8115c0ed75c88a7df522b83
Nam Bokyeong <bo_bo_@kakao.com>
2023-10-14 13:46:54 +0900

    임시 commit

--- ZZin/InfoViewController.swift
+++ ZZin/InfoViewController.swift
@@ -7,23 +7,71 @@
 
 import UIKit
 
-class InfoViewController: UIViewController {
-
+class InfoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        // Do any additional setup after loading the view.
+        
+        self.title = "마이페이지"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // TableView 설정
+    
+    func numberOfSections(in tableView: UITableView) -> Int {2}
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0: return 1
+        case 1: return 1 // 데이터 수에 맞게 변경
+        default: return 1
+        }
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.section {
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: UserInfoTableViewCell.identifier) as! UserInfoTableViewCell
+            return cell
+        case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: InfoLocalListTableViewCell.identifier) as! InfoLocalListTableViewCell
+            return cell
+        default: return UITableViewCell()
+        }
+    }
 
 }
+
+class UserInfoTableViewCell: UITableViewCell {
+    static let identifier = "userInfoCell"
+    
+}
+// collectionView가 TableViewCell이 되는 부분
+class InfoLocalListTableViewCell: UITableViewCell, UICollectionViewDataSource, UICollectionViewDelegate {
+    static let identifier = "localListTableViewCell"
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {1}
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+}
+
+// dummy
+let zzinBasicInfos : [ZZinBasicInfo] = [zzin1, zzin2, zzin3, zzin4, zzin5, zzin6, zzin7, zzin8]
+
+let zzin1 : ZZinBasicInfo = ZZinBasicInfo(registerDate: Date(), id: UUID(), name: "오구당당 부평본점", address: "인천 부평구 경원대로1377번길 47", lat: 126.722032, long: 37.4934269, RepresentativeImageURL: "ogudangdang")
+let zzin2 : ZZinBasicInfo = ZZinBasicInfo(registerDate: Date(), id: UUID(), name: "오구당당 부평본점", address: "인천 부평구 경원대로1377번길 47", lat: 126.722032, long: 37.4934269, RepresentativeImageURL: "ogudangdang")
+let zzin3 : ZZinBasicInfo = ZZinBasicInfo(registerDate: Date(), id: UUID(), name: "오구당당 부평본점", address: "인천 부평구 경원대로1377번길 47", lat: 126.722032, long: 37.4934269, RepresentativeImageURL: "ogudangdang")
+let zzin4 : ZZinBasicInfo = ZZinBasicInfo(registerDate: Date(), id: UUID(), name: "오구당당 부평본점", address: "인천 부평구 경원대로1377번길 47", lat: 126.722032, long: 37.4934269, RepresentativeImageURL: "ogudangdang")
+let zzin5 : ZZinBasicInfo = ZZinBasicInfo(registerDate: Date(), id: UUID(), name: "오구당당 부평본점", address: "인천 부평구 경원대로1377번길 47", lat: 126.722032, long: 37.4934269, RepresentativeImageURL: "ogudangdang")
+let zzin6 : ZZinBasicInfo = ZZinBasicInfo(registerDate: Date(), id: UUID(), name: "오구당당 부평본점", address: "인천 부평구 경원대로1377번길 47", lat: 126.722032, long: 37.4934269, RepresentativeImageURL: "ogudangdang")
+let zzin7 : ZZinBasicInfo = ZZinBasicInfo(registerDate: Date(), id: UUID(), name: "오구당당 부평본점", address: "인천 부평구 경원대로1377번길 47", lat: 126.722032, long: 37.4934269, RepresentativeImageURL: "ogudangdang")
+let zzin8 : ZZinBasicInfo = ZZinBasicInfo(registerDate: Date(), id: UUID(), name: "오구당당 부평본점", address: "인천 부평구 경원대로1377번길 47", lat: 126.722032, long: 37.4934269, RepresentativeImageURL: "ogudangdang")
+
+let zzinRecommandations : [ZZinRecommendation] = [zzinReccmmandation1, zzinReccmmandation2, zzinReccmmandation3, zzinReccmmandation4]
+
+let zzinReccmmandation1 : ZZinRecommendation = ZZinRecommendation(basicInfo: zzin1, companys: .withFamily, conditions: .lotsOfSideDishes, category: .korean, imageURL: "ogudangdang_review")
+let zzinReccmmandation2 : ZZinRecommendation = ZZinRecommendation(basicInfo: zzin1, companys: .withFamily, conditions: .lotsOfSideDishes, category: .korean, imageURL: "ogudangdang_review")
+let zzinReccmmandation3 : ZZinRecommendation = ZZinRecommendation(basicInfo: zzin1, companys: .withFamily, conditions: .lotsOfSideDishes, category: .korean, imageURL: "ogudangdang_review")
+let zzinReccmmandation4 : ZZinRecommendation = ZZinRecommendation(basicInfo: zzin1, companys: .withFamily, conditions: .lotsOfSideDishes, category: .korean, imageURL: "ogudangdang_review")
---
+++ ZZin/Model.swift
@@ -0,0 +1,49 @@
+//
+//  Model.swift
+//  ZZin
+//
+//  Created by 보경 on 10/13/23.
+//
+
+import Foundation
+
+enum Company: String {
+    case withFamily = "⭐️가족과 함께"
+    case withFriends = "😎친구와 함께"
+    case withCoworkers = "💪동료와 함께"
+    case alone = "😋혼밥"
+}
+
+enum Condition: String {
+    case rainy = "☔️비가 오는 날엔"
+    case luxurious = "💰고급 음식점"
+    case oldStoreVibe = "🏚️노포 바이브"
+    case lotsOfSideDishes = "🧆반찬 많이요"
+}
+
+enum Category: String {
+    case korean = "한식"
+    case chinese = "중식"
+    case japanese = "일식"
+    case western = "양식"
+    case boonsik = "분식"
+    case coffeeAndDessert = "카페&디저트"
+}
+
+struct ZZinBasicInfo {
+    var registerDate: Date
+    var id: UUID
+    var name: String
+    var address: String
+    var lat: Double
+    var long: Double
+    var RepresentativeImageURL: String
+}
+
+struct ZZinRecommendation {
+    var basicInfo: ZZinBasicInfo
+    var companys: Company
+    var conditions: Condition
+    var category: Category
+    var imageURL: String
+}
