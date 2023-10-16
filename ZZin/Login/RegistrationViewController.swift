980698fefa85ce4b9ed3ded949eb8e2890d57b9c
Jack Lee <dongjunlee222@gmail.com>
2023-10-16 23:58:14 +0900

    [FEAT]: ⚙️ 회원가입 페이지 일부 구현

--- ZZin/Login/RegistrationViewController.swift
+++ ZZin/Login/RegistrationViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class RegistrationViewController: UIViewController {
     
@@ -42,8 +43,8 @@ class RegistrationViewController: UIViewController {
         view.backgroundColor = .gray
         view.layer.cornerRadius = 12
         view.clipsToBounds = true
-        view.addSubview(idTextField)
-        view.addSubview(idInfoLabel)
+        view.addSubview(pwTextField)
+        view.addSubview(pwInfoLabel)
         return view
     }()
     
@@ -65,16 +66,195 @@ class RegistrationViewController: UIViewController {
         return tf
     }()
     
+    private lazy var checkPwTextfieldView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray
+        view.layer.cornerRadius = 12
+        view.clipsToBounds = true
+        view.addSubview(checkPwTextField)
+        view.addSubview(checkPwInfoLabel)
+        return view
+    }()
+    
+    private var checkPwInfoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "한번만 더 입력해 주세요"
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private var checkPwTextField: UITextField = {
+        var tf = UITextField()
+        tf.backgroundColor = .blue
+        tf.textColor = .white
+        tf.keyboardType = .default
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+    
+    private lazy var nicknameTextfieldView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray
+        view.layer.cornerRadius = 12
+        view.clipsToBounds = true
+        view.addSubview(nicknameTextField)
+        view.addSubview(nicknameInfoLabel)
+        return view
+    }()
+    
+    private var nicknameInfoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "닉네임을 입력해 주세요"
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private var nicknameTextField: UITextField = {
+        var tf = UITextField()
+        tf.backgroundColor = .blue
+        tf.textColor = .white
+        tf.keyboardType = .default
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+    
+    private lazy var numberTextfieldView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray
+        view.layer.cornerRadius = 12
+        view.clipsToBounds = true
+        view.addSubview(numberTextField)
+        view.addSubview(numberInfoLabel)
+        return view
+    }()
+    
+    private var numberInfoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "전화 번호를 입력해주세요"
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private var numberTextField: UITextField = {
+        var tf = UITextField()
+        tf.backgroundColor = .blue
+        tf.textColor = .white
+        tf.keyboardType = .default
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+    
+    private var doubleCheckButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("중복 확인", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+        return button
+    }()
+    
     func configure() {
         view.backgroundColor = .yellow
-                
-        [idTextfieldView].forEach{view.addSubview($0)}
+        [idTextfieldView, pwTextfieldView, checkPwTextfieldView, nicknameTextfieldView, numberTextfieldView, doubleCheckButton].forEach{view.addSubview($0)}
     }
     
-    func setUI() {
-        idTextfieldView.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(184)
+    func setIdTextfield() {
+        idTextfieldView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(184)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(52)
+            make.width.equalTo(353)
+        }
+        
+        idInfoLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(5)
+        }
+        
+        idTextField.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(5)
+        }
+    }
+    
+    func setPwTextField() {
+        
+        pwTextfieldView.snp.makeConstraints { make in
+            make.top.equalTo(idTextfieldView.snp.bottom).offset(40)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(52)
+            make.width.equalTo(353)
+        }
+        
+        pwInfoLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(5)
+        }
+        
+        pwTextField.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(5)
+        }
+        
+        checkPwTextfieldView.snp.makeConstraints { make in
+            make.top.equalTo(pwTextfieldView.snp.bottom).offset(40)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(52)
+            make.width.equalTo(353)
+        }
+        
+        checkPwInfoLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(5)
+        }
+        
+        checkPwTextField.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(5)
+        }
+    }
+    
+    func setNicknameTextfield() {
+        
+        nicknameTextfieldView.snp.makeConstraints {
+            $0.top.equalTo(checkPwTextfieldView.snp.bottom).offset(50)
             $0.centerX.equalToSuperview()
+            $0.height.equalTo(52)
+            $0.width.equalTo(353)
+        }
+        
+        nicknameInfoLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(5)
+        }
+        
+        nicknameTextField.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(5)
+        }
+    }
+    
+    func setNumberTextfield() {
+        numberTextfieldView.snp.makeConstraints {
+            $0.top.equalTo(nicknameTextfieldView.snp.bottom).offset(40)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(52)
+            $0.width.equalTo(353)
+        }
+        
+        numberInfoLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(5)
+        }
+        
+        numberTextField.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(5)
         }
     }
     
@@ -88,6 +268,9 @@ extension RegistrationViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configure()
-        setUI()
+        setIdTextfield()
+        setPwTextField()
+        setNicknameTextfield()
+        setNumberTextfield()
     }
 }
