b974d6ebc52391d668e238bf80682ba1bc19bf77
juyeon jeong <d9yarn@gmail.com>
2023-10-17 15:50:02 +0900

    [FEAT]: 네비게이션 바 아이템 설정

---
+++ ZZin/SearchPage/SearchResultCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResultCollectionViewCell.swift
+//  ZZin
+//
+//  Created by t2023-m0045 on 10/17/23.
+//
+
+import UIKit
+
+class SearchResultCollectionViewCell: UICollectionViewCell {
+    
+}
