6a33ed4d5578888d35052cf1f60ce8b279fe040b
Jack Lee <dongjunlee222@gmail.com>
2023-10-23 14:05:42 +0900

    [FIX]: 🔧 사용자 데이터 싱글톤 처리이후 병합

--- ZZin/Login/Utility/AuthService.swift
+++ ZZin/Login/Utility/AuthService.swift
@@ -27,10 +27,10 @@ class AuthService {
     ///   - Bool: wasRegistered - 유저가 데이터베이스에 저장이 되었는지 확인
     ///   - Error?: 에러가 발생할 경우 활용하는 Error
     func registerUser(with user: User, completion: @escaping (Bool, Error?) -> Void) {
-        let email = user.id
-        let password = user.password
+        let email = user.uid
+        let password = user.password ?? ""
         let username = user.nickname
-        
+
         Auth.auth().createUser(withEmail: email, password: password) { result, error in
             if let error = error {
                 completion(false, error)
@@ -56,7 +56,7 @@ class AuthService {
     }
     
     func signIn(with user: User) {
-        Auth.auth().signIn(withEmail: user.id, password: user.password) { result, error in
+        Auth.auth().signIn(withEmail: user.uid, password: user.password ?? "") { result, error in
             if let error = error {
                 print("로그인하는데 문제가 발생했어요!")
             }
