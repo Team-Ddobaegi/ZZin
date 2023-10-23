77586671464a5b4e0784f2143a0387404df9af48
Jack Lee <dongjunlee222@gmail.com>
2023-10-23 09:52:27 +0900

    [HOTFIX]: 💥 유저 생성 및 로그인 처리 에러 발생

--- ZZin/Login/RegistrationViewController.swift
+++ ZZin/Login/RegistrationViewController.swift
@@ -8,8 +8,8 @@
 import UIKit
 import SnapKit
 import Then
-import FirebaseAuth
-import Firebase
+//import FirebaseAuth
+//import Firebase
 
 class RegistrationViewController: UIViewController {
     
@@ -299,54 +299,49 @@ class RegistrationViewController: UIViewController {
         }
         // 비밀번호가 안전한지 확인 - 8자 이상, 대문자 / 소문자 / 숫자 조합인지 확인
         let checkedPassword = pwTextField.text!
-        if validatePassword(checkedPassword) == false {
+        if AuthService.shared.validatePassword(checkedPassword) == false {
             print("비밀번호가 8자 이상, 숫자와 특수문자가 최소 1개씩 들어갔는지 확인해주세요!")
             return "비밀번호가 8자 이상, 숫자와 특수문자가 최소 1개씩 들어갔는지 확인해주세요!"
         }
         return nil
     }
     
-    func validatePassword(_ password: String) -> Bool {
-        let passwordCheck = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8}$"
-        let predicate = NSPredicate(format:"SELF MATCHES %@", passwordCheck)
-        return predicate.evaluate(with: password)
-    }
-    
+    // 여기서 문제점은 한 버튼에 모든 역할을 부여했기에 코드를 이해하는데 발생하는 이슈
     @objc func confirmButtonTapped() {
         print("확인 버튼이 눌렸습니다.")
         
-        
-        
-        
-//        // 값이 모두 입력됐는지 확인
-//        let error = validateData()
-//        if error != nil {
-//            // validate 함수에서 에러가 발생!
-//            print("에러가 발생했습니다. \(error)")
-//        } else {
-//            // clean 아이디와 비밀번호가 있는지 확인 완료
-//            let checkedId = idTextField.text!
-//            let checkedPassword = pwTextField.text!
-//            
-//            // 유저 생성 - Firebase가 가지고 있는 함수를 활용해서 사용자 생성
-//            Auth.auth().createUser(withEmail: checkedId, password: checkedPassword) { result, error in
-//                guard let error = error else { print("에러가 발생했습니다. - \(error?.localizedDescription)"); return }
-//                // 유저가 에러 없었다! 생성이 되야 한다는 점
-//                let db = Firestore.firestore()
-//                db.collection("users").addDocument(data: ["id": checkedId, "password": checkedPassword, "uid": result?.user.uid]) { error in
-//                    // 이 단계에서 데이터 저장 관련 에러를 확인하는 과정을 거치는게 맞나? >> error != nil이 맞나?
-//                    if error != nil {
-//                        print("저장하는데 에러가 발생했습니다.")
-//                    }
-//                }
-//            }
-//            print("회원가입이 완료되었습니다.")
-//            let vc = LoginViewController()
-//            vc.modalPresentationStyle = .fullScreen
-//            self.present(vc, animated: true)
-//        }
-//        // 화면 전환
-//        self.dismiss(animated: true)
+        // 값이 모두 입력됐는지 확인
+        let error = validateData()
+        if error != nil {
+            // validate 함수에서 에러가 발생!
+            print("에러가 발생했습니다. \(error)")
+        } else {
+            // clean 아이디와 비밀번호가 있는지 확인 완료
+            let checkedId = idTextField.text!
+            let checkedPassword = pwTextField.text!
+            
+            //            AuthService.shared.registerUser(with: checked, completion: <#T##(Bool, Error?) -> Void#>)
+            
+            //            // 유저 생성 - Firebase가 가지고 있는 함수를 활용해서 사용자 생성
+            //            Auth.auth().createUser(withEmail: checkedId, password: checkedPassword) { result, error in
+            //                guard let error = error else { print("에러가 발생했습니다. - \(error?.localizedDescription)"); return }
+            //                // 유저가 에러 없었다! 생성이 되야 한다는 점
+            //                let db = Firestore.firestore()
+            //                db.collection("users").addDocument(data: ["id": checkedId, "password": checkedPassword, "uid": result?.user.uid]) { error in
+            //                    // 이 단계에서 데이터 저장 관련 에러를 확인하는 과정을 거치는게 맞나? >> error != nil이 맞나?
+            //                    if error != nil {
+            //                        print("저장하는데 에러가 발생했습니다.")
+            //                    }
+            //                }
+            //            }
+            //            print("회원가입이 완료되었습니다.")
+            //            let vc = LoginViewController()
+            //            vc.modalPresentationStyle = .fullScreen
+            //            self.present(vc, animated: true)
+            //        }
+            //        // 화면 전환
+            //        self.dismiss(animated: true)
+        }
     }
     
     @objc func doubleCheckButtonTapped() {
