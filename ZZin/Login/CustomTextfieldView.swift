6d94cc2a25d2c564b07c19adb1dec170ddf06569
Jack Lee <dongjunlee222@gmail.com>
2023-10-26 01:18:59 +0900

    [FIX]: 🔧 Custom TextfieldView 함수 및 구현 일부 수정

--- ZZin/Login/CustomTextfieldView.swift
+++ ZZin/Login/CustomTextfieldView.swift
@@ -10,21 +10,27 @@ import SnapKit
 import Then
 
 class CustomTextfieldView: UIView {
-
+    
     let animatingLabel = UILabel().then {
         $0.translatesAutoresizingMaskIntoConstraints = false
         $0.textColor = .black
-        $0.text = "테스트 값"
+        $0.font = UIFont.systemFont(ofSize: 14)
+        $0.text = "Test"
     }
 
     let textfield = UITextField().then {
         $0.translatesAutoresizingMaskIntoConstraints = false
         $0.tintColor = .systemGreen
-        $0.placeholder = "텍스트 값"
+        $0.autocapitalizationType = .none
+        $0.autocorrectionType = .no
+        $0.keyboardType = .default
     }
 
-    let cancleButton = UIButton().then {
-        let image = UIImage(systemName: "x.square.fill")
+    let cancelButton = UIButton().then {
+        let image = UIImage(systemName: "x.circle")
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.imageView?.tintColor = .systemGray
+        $0.isHidden = true
         $0.setImage(image, for: .normal)
         $0.addTarget(self, action: #selector(cancelTapped), for: .touchUpInside)
     }
@@ -51,9 +57,9 @@ extension CustomTextfieldView {
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = .systemGray5
         layer.cornerRadius = 12
-        [animatingLabel, textfield].forEach{ addSubview($0) }
+        [animatingLabel, textfield, cancelButton].forEach{ addSubview($0) }
 
-        let tap = UITapGestureRecognizer(target: self, action: #selector(labelTapped))
+        let tap = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
         addGestureRecognizer(tap)
     }
 
@@ -67,6 +73,12 @@ extension CustomTextfieldView {
             $0.leading.equalToSuperview().inset(5)
             $0.trailing.equalToSuperview().inset(5)
             $0.centerY.equalToSuperview()
+        }
+        
+        cancelButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(5)
+            $0.height.width.equalTo(30)
         }
     }
 
@@ -87,6 +99,8 @@ extension CustomTextfieldView {
         } completion: { position in
             self.textfield.isHidden = false
             self.textfield.becomeFirstResponder()
+            
+            self.cancelButton.isHidden = false
         }
     }
 
@@ -98,16 +112,24 @@ extension CustomTextfieldView {
             self.layer.borderColor = UIColor.clear.cgColor
 
             // visibility
-            self.textfield.isHidden = true
             self.textfield.text = ""
+            self.textfield.isHidden = true
+            self.cancelButton.isHidden = true
 
             // movement
             self.animatingLabel.transform = .identity
         }
         movement.startAnimation()
     }
+    
+    static func create(placeholder: String, text: String) -> CustomTextfieldView {
+        let customtf = CustomTextfieldView()
+        customtf.animatingLabel.text = text
+        customtf.textfield.placeholder = placeholder
+        return customtf
+    }
 
-    @objc func labelTapped(_ recognizer: UITapGestureRecognizer) {
+    @objc func viewTapped(_ recognizer: UITapGestureRecognizer) {
         if recognizer.state == .ended {
             print("탭이 종료되었습니다.")
             labelAnimation()
