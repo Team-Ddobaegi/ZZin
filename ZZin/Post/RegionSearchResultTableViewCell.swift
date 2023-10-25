305f957a08b33329022f0721d420f60a5e3cb524
bo_bo_ <bo_bo_@kakao.com>
2023-10-25 11:02:49 +0900

    [ADD] : 💾 RegionSearchResultTableViewCell 파일 추가

---
+++ ZZin/Post/RegionSearchResultTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RegionSearchResultTableViewCell.swift
+//  ZZin
+//
+//  Created by t2023-m0055 on 2023/10/25.
+//
+
+import UIKit
+
+class RegionSearchResultTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
